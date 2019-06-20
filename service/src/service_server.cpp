776cda1dd8c6622029b18b6dbd37f85ecd910a50
Jaehoon <pf7@naver.com>
2019-06-20 16:33:15 +0900

    parameter test(service pkg)

--- service/src/service_server.cpp
+++ service/src/service_server.cpp
@@ -1,11 +1,42 @@
 #include "ros/ros.h"
 #include "service/Service1.h"
 
+#define PLUS	1
+#define MINUS	2
+#define MULT	3
+#define DIVS	4
+
+int g_operator = PLUS;
+
+
 bool Servercb(service::Service1::Request& req,
 		service::Service1::Response& res){
+
+
+	switch(g_operator)
+	{
+	default:
+	case PLUS:
 	res.result = req.a + req.b;
+	break;
+	case MINUS:
+	res.result = req.a - req.b;
+	break;
+	case MULT:
+	res.result = req.a * req.b;
+	break;
+	case DIVS:
+	if(req.b == 0){
+	res.result = 0;
+	}
+	else{
+	res.result = req.a/req.b;
+	}
+	break;
+	}
+		
 	ROS_INFO("request: x = %ld, y = %ld", (long int)req.a, (long int)req.b);
-	ROS_INFO("result : x+y = %ld", (long int)res.result);
+	ROS_INFO("result : f(x,y) = %ld", (long int)res.result);
 	return true;
 }
 
@@ -15,11 +46,19 @@ int main(int argc, char** argv){
 	ros::init(argc, argv, "service_server_name");
 	ros::NodeHandle nh;
 
+	ros::Rate r(10);
+
+	nh.setParam("cal_method", PLUS);
+
 	ros::ServiceServer service_server = nh.advertiseService("service_topic_name", Servercb);
 
 	ROS_INFO("ready service server!");
 
-	ros::spin();
+	while(1){
+	nh.getParam("cal_method", g_operator);	
+	ros::spinOnce();
+	r.sleep();
 
+	}
 	return 0;
 }
